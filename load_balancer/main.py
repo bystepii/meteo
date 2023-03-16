6e89fc983ebb01a260cb21cd777d7c76ca0eff0e
Stepan Klymonchuk <stepanstepan0000@gmail.com>
2023-03-16 17:50:28 +0100

    Update logging

--- load_balancer/main.py
+++ load_balancer/main.py
@@ -5,10 +5,10 @@ from concurrent import futures
 import grpc
 
 from common.log import setup_logger
-from load_balancer.load_balancer import LoadBalancer
+from common.registration_service import RegistrationService
+from load_balancer.lb import LoadBalancer
 from load_balancer.meteo_service import MeteoService
 from load_balancer.meteo_service_servicer import MeteoServiceServicer
-from load_balancer.registration_service import RegistrationService
 from load_balancer.registration_servicer import RegistrationServiceServicer
 from proto.services.meteo import meteo_service_pb2_grpc
 from proto.services.registration import registration_service_pb2_grpc
@@ -18,33 +18,42 @@ logger = logging.getLogger(__name__)
 
 def main():
     # Create a gRPC server
+    logger.info("Creating gRPC server")
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
 
-    # Create load balancer
-    load_balancer = LoadBalancer()
+    logger.info("Creating services")
 
     # Create RegistrationService
-    registration_service = RegistrationService()
+    registration_service = RegistrationService(parent_service="LoadBalancer")
+
+    # Create load balancer
+    load_balancer = LoadBalancer(registration_service)
 
     # Create MeteoService
     meteo_service = MeteoService(load_balancer, registration_service)
 
     # Register the MeteoService
+    logger.info("Registering MeteoServiceServicer")
     meteo_service_pb2_grpc.add_MeteoServiceServicer_to_server(
         MeteoServiceServicer(meteo_service),
         server
     )
 
     # Register the RegistrationService
+    logger.info("Registering RegistrationServiceServicer")
     registration_service_pb2_grpc.add_RegistrationServiceServicer_to_server(
         RegistrationServiceServicer(registration_service),
         server
     )
 
     # Listen on port 50051
+    logger.info("Starting gRPC server")
     server.add_insecure_port('[::]:50051')
     server.start()
 
+    logger.info("gRPC server started successfully")
+
+    # Keep the server running
     try:
         while True:
             time.sleep(86400)
--- load_balancer/meteo_service_servicer.py
+++ load_balancer/meteo_service_servicer.py
@@ -1,18 +1,25 @@
+import logging
+
 from google.protobuf.empty_pb2 import Empty
 
 from load_balancer.meteo_service import MeteoService
 from proto.messages.meteo.meteo_messages_pb2 import RawMeteoData, RawPollutionData
 from proto.services.meteo import meteo_service_pb2_grpc
 
+logger = logging.getLogger(__name__)
+
 
 class MeteoServiceServicer(meteo_service_pb2_grpc.MeteoServiceServicer):
     def __init__(self, meteo_service: MeteoService):
+        logger.info("Initializing MeteoServiceServicer")
         self._meteo_service = meteo_service
 
     def SendMeteoData(self, meteo_data: RawMeteoData, context) -> Empty:
+        logger.debug(f'Received meteo data {meteo_data}')
         self._meteo_service.send_meteo_data(meteo_data)
         return Empty()
 
     def SendPollutionData(self, pollution_data: RawPollutionData, context) -> Empty:
+        logger.debug(f'Received pollution data {pollution_data}')
         self._meteo_service.send_pollution_data(pollution_data)
         return Empty()
