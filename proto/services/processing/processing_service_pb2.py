f8b73f186e8a54eb19735c0469cc868af3c54a65
Stepan Klymonchuk <stepanstepan0000@gmail.com>
2023-03-23 11:39:11 +0100

    Update proto definition

--- proto/services/processing/processing_service_pb2.py
+++ proto/services/processing/processing_service_pb2.py
@@ -11,17 +11,20 @@ from google.protobuf import symbol_database as _symbol_database
 _sym_db = _symbol_database.Default()
 
 
-from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 from proto.messages.meteo import meteo_messages_pb2 as proto_dot_messages_dot_meteo_dot_meteo__messages__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n2proto/services/processing/processing_service.proto\x12\x05meteo\x1a\x1bgoogle/protobuf/empty.proto\x1a)proto/messages/meteo/meteo_messages.proto2\x9d\x01\n\x11ProcessingService\x12?\n\x10ProcessMeteoData\x12\x13.meteo.RawMeteoData\x1a\x16.google.protobuf.Empty\x12G\n\x14ProcessPollutionData\x12\x17.meteo.RawPollutionData\x1a\x16.google.protobuf.Emptyb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n2proto/services/processing/processing_service.proto\x12\x05meteo\x1a)proto/messages/meteo/meteo_messages.proto\"\'\n\x16\x41irWellnessCoefficient\x12\r\n\x05value\x18\x01 \x01(\x02\"%\n\x14PollutionCoefficient\x12\r\n\x05value\x18\x01 \x01(\x02\x32\xa9\x01\n\x11ProcessingService\x12\x46\n\x10ProcessMeteoData\x12\x13.meteo.RawMeteoData\x1a\x1d.meteo.AirWellnessCoefficient\x12L\n\x14ProcessPollutionData\x12\x17.meteo.RawPollutionData\x1a\x1b.meteo.PollutionCoefficientb\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'proto.services.processing.processing_service_pb2', globals())
 if _descriptor._USE_C_DESCRIPTORS == False:
 
   DESCRIPTOR._options = None
-  _PROCESSINGSERVICE._serialized_start=134
-  _PROCESSINGSERVICE._serialized_end=291
+  _AIRWELLNESSCOEFFICIENT._serialized_start=104
+  _AIRWELLNESSCOEFFICIENT._serialized_end=143
+  _POLLUTIONCOEFFICIENT._serialized_start=145
+  _POLLUTIONCOEFFICIENT._serialized_end=182
+  _PROCESSINGSERVICE._serialized_start=185
+  _PROCESSINGSERVICE._serialized_end=354
 # @@protoc_insertion_point(module_scope)
--- proto/services/processing/processing_service_pb2.pyi
+++ proto/services/processing/processing_service_pb2.pyi
@@ -1,6 +1,18 @@
-from google.protobuf import empty_pb2 as _empty_pb2
 from proto.messages.meteo import meteo_messages_pb2 as _meteo_messages_pb2
 from google.protobuf import descriptor as _descriptor
-from typing import ClassVar as _ClassVar
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
 
 DESCRIPTOR: _descriptor.FileDescriptor
+
+class AirWellnessCoefficient(_message.Message):
+    __slots__ = ["value"]
+    VALUE_FIELD_NUMBER: _ClassVar[int]
+    value: float
+    def __init__(self, value: _Optional[float] = ...) -> None: ...
+
+class PollutionCoefficient(_message.Message):
+    __slots__ = ["value"]
+    VALUE_FIELD_NUMBER: _ClassVar[int]
+    value: float
+    def __init__(self, value: _Optional[float] = ...) -> None: ...
