ee017b490e2ea68cf478cf6e2c66004c64f2186a
Stepan Klymonchuk <stepanstepan0000@gmail.com>
2023-04-06 20:31:12 +0200

    Add timestamp to results

--- proto/services/terminal/terminal_service_pb2.py
+++ proto/services/terminal/terminal_service_pb2.py
@@ -12,17 +12,18 @@ _sym_db = _symbol_database.Default()
 
 
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
+from google.protobuf import timestamp_pb2 as google_dot_protobuf_dot_timestamp__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n.proto/services/terminal/terminal_service.proto\x12\x08terminal\x1a\x1bgoogle/protobuf/empty.proto\"8\n\x07Results\x12\x15\n\rwellness_data\x18\x01 \x01(\x02\x12\x16\n\x0epollution_data\x18\x02 \x01(\x02\x32K\n\x0fTerminalService\x12\x38\n\x0bSendResults\x12\x11.terminal.Results\x1a\x16.google.protobuf.Emptyb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n.proto/services/terminal/terminal_service.proto\x12\x08terminal\x1a\x1bgoogle/protobuf/empty.proto\x1a\x1fgoogle/protobuf/timestamp.proto\"\xa9\x01\n\x07Results\x12\x15\n\rwellness_data\x18\x01 \x01(\x02\x12\x36\n\x12wellness_timestamp\x18\x02 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12\x16\n\x0epollution_data\x18\x03 \x01(\x02\x12\x37\n\x13pollution_timestamp\x18\x04 \x01(\x0b\x32\x1a.google.protobuf.Timestamp2K\n\x0fTerminalService\x12\x38\n\x0bSendResults\x12\x11.terminal.Results\x1a\x16.google.protobuf.Emptyb\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'proto.services.terminal.terminal_service_pb2', globals())
 if _descriptor._USE_C_DESCRIPTORS == False:
 
   DESCRIPTOR._options = None
-  _RESULTS._serialized_start=89
-  _RESULTS._serialized_end=145
-  _TERMINALSERVICE._serialized_start=147
-  _TERMINALSERVICE._serialized_end=222
+  _RESULTS._serialized_start=123
+  _RESULTS._serialized_end=292
+  _TERMINALSERVICE._serialized_start=294
+  _TERMINALSERVICE._serialized_end=369
 # @@protoc_insertion_point(module_scope)
--- proto/services/terminal/terminal_service_pb2.pyi
+++ proto/services/terminal/terminal_service_pb2.pyi
@@ -1,14 +1,19 @@
 from google.protobuf import empty_pb2 as _empty_pb2
+from google.protobuf import timestamp_pb2 as _timestamp_pb2
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar as _ClassVar, Optional as _Optional
+from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class Results(_message.Message):
-    __slots__ = ["pollution_data", "wellness_data"]
+    __slots__ = ["pollution_data", "pollution_timestamp", "wellness_data", "wellness_timestamp"]
     POLLUTION_DATA_FIELD_NUMBER: _ClassVar[int]
+    POLLUTION_TIMESTAMP_FIELD_NUMBER: _ClassVar[int]
     WELLNESS_DATA_FIELD_NUMBER: _ClassVar[int]
+    WELLNESS_TIMESTAMP_FIELD_NUMBER: _ClassVar[int]
     pollution_data: float
+    pollution_timestamp: _timestamp_pb2.Timestamp
     wellness_data: float
-    def __init__(self, wellness_data: _Optional[float] = ..., pollution_data: _Optional[float] = ...) -> None: ...
+    wellness_timestamp: _timestamp_pb2.Timestamp
+    def __init__(self, wellness_data: _Optional[float] = ..., wellness_timestamp: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., pollution_data: _Optional[float] = ..., pollution_timestamp: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ...) -> None: ...
