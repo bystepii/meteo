7f9f244df00aa9786113f4047f767474876195d4
Stepan Klymonchuk <stepanstepan0000@gmail.com>
2023-04-07 13:24:42 +0200

    Remove unused import

--- terminal/terminal_service.py
+++ terminal/terminal_service.py
@@ -1,7 +1,6 @@
 import logging
 
 from common.log import format_proto_msg
-from proto.messages.meteo.meteo_messages_pb2 import RawMeteoData, RawPollutionData
 from proto.services.terminal.terminal_service_pb2 import Results
 
 logger = logging.getLogger(__name__)
